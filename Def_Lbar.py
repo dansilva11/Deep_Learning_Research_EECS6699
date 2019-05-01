8a92bb13c370240d61d9698a3f523bfc0368fa30
rvv2106 <45322508+rvv2106@users.noreply.github.com>
2019-05-01 19:29:32 -0400

    Updated Def_Lbar.py getLWeights() and customWeights(). customWeights() returns a resultant Lbar, rather than using Lbar as input.

--- Def_Lbar.py
+++ Def_Lbar.py
@@ -11,79 +11,74 @@ from Def_Model import initNodes
 # -- Variable Definitions -- 
 #   d = Input Data Dimensions
 #   L = Network Depth (# layers, excl. Input Layer)
+#   hL= Number of hidden layers in a network (excl. Input and Output)
 #   W = # Parameters
 #   w_s = List of # Hidden Nodes/Layer (Custom value per Layer)
-#   sum_weights = Array of incremental sum of weights in previous layers 
 
-def getLWeights(L, w_s, d):
 
-	# Setup 1-D array to sum weights across all layers
-	layer_weights = np.ones(L)
+#d and L are assumed to be positive integers, w_s is an array of positive integers 
+def getLWeights(d,L,w_s): 
+    #returns an array of input parameters per layer 
+    
+    # Setup 1-D array to sum weights across all layers
+    #   set an extra layer to grab the output weights
+    layer_weights = np.ones(L+1) 
+    
+    layer_weights[0] = d*w_s[0] #first layer takes from input
+    
+    # Define # weights/layer and incrementally sum weights across prev layers
+    for i in range(L+1):  
+        #move from second layer through to the last hidden layer 
+        if (i > 0) and (i <= L-1): 
+            # Weights per Hidden Layer = # Nodes in Prev Layer (+1 for Bias) x # Nodes in Curr Layer
+            layer_weights[i] = (w_s[i-1]+1)*w_s[i]
+        #once you are in the output layer, count the nodes plus bias 
+        elif(i > L-1): 
+            layer_weights[i] = w_s[i-1]+1
+            
+    return layer_weights
 
-	# Define # nodes for input layer
-	layer_weights[0] = d*w_s[0]
 
-	# Define # weights/layer and incrementally sum weights across prev layers
-	for i in range(L):
-		if (0 < i < L-2):
-			# Weights per Hidden Layer = # Nodes in Prev Layer (+1 for Bias) x # Nodes in Curr Layer
-			layer_weights[i] = (w_s[i-1]+1)*w_s[i]
-	
-	# Increment total # weights from last layer to output
-	layer_weights += w_s[i]
-	
-	return layer_weights
+#all inputs are assumed to be positive integers 
+def customWeights(hL,W,d,cL,minw):
+    np.set_printoptions(suppress=True)
+    print("Generating network with concentrated layer ",str(cL))
+    
+    # Initialize 1-D array of hidden nodes per layer
+    layer_nodes = np.ones(hL) 
 
-def customWeights(L, W, d, Lbar, pace):
-
-	# Initialize 1-D array of hidden nodes per layer
-	w_s = np.ones(L) 
-
-	# Start with evenly distributed nodes
-	w_i = initNodes(L, W, d)
-	w_s = w_s * w_i
-
-	# Setup 1-D Sum of total # weights in network
-	layer_weights = getLWeights(L, w_s, d)
-
-	np.set_printoptions(suppress=True)
-
-	# Cumulative sum of flattened 
-	mylbar = round(sum(np.cumsum(sum_weights)/sum(sum_weights)))
-	print('Starting Weights: ',str(sum(sum_weights)))
-	notReady = True
-	
-	if Lbar == mylbar:
-		return w_s
-	else:
-		while(notReady):
-			#print('NotReady:: moving lbar ',str(mylbar),' to target ',str(Lbar))
-			#print(w_s)
-			cL = L-Lbar #concentrated layer
-			#if mylbar != Lbar: 
-			deltas = pace*np.ones(L) #take pace nodes from each layer
-			deltas[cL] = 0 #dont take nodes from cL
-			for i in range(L):
-				if(0 >= w_s[i] - deltas[i]): #if too many nodes taken, keep at least 1
-					deltas[i] = w_s[i]-1
-				w_s[i] = max(1,w_s[i]-deltas[i])
-			
-			if(sum(deltas)==0): #add pace nodes to cL if non taken
-				w_s[cL] += pace    
-			w_s[cL] += sum(deltas) #add balance of nodes onto cL
-			
-			sum_weights = getLWeights(L, w_s, d)
-			if(sum(sum_weights) < W): #if there are still weights to be added add a node to all layers
-				for i in range(L): w_s[i] += 1 
-			if(sum(sum_weights) > 1.10*W): #if there are too many weights
-				for i in range(L): w_s[i] = max(1,w_s[i]-1) 
-			sum_weights = getLWeights(L, w_s, d)
-			print('Current Weights: ',str(sum(sum_weights)))
-				
-				
-			mylbar = round(sum(np.cumsum(sum_weights)/sum(sum_weights)))
-			if((mylbar == Lbar) & ((sum(sum_weights)/W - 1) > -0.01)):
-				notReady = False
-				print('Total Weights: ',str(sum(sum_weights)),' with Lbar: ',str(mylbar))
-				return w_s                    
-	return w_s
+    # With a single hidden layer, number of nodes is found in a single term
+    if(hL==1):
+        cW = round((W-1)/(d+1))
+    # With multiple hidden layers, the solution depends on the concentrated layer 
+    #   use closed form solution to approximate root 
+    else: 
+        if(cL==1): # When the concentraded layer is the first hidden layer
+            cW = round((W - max(0,hL-3)*(minw+1)*minw - (minw+1) - minw)/(d + minw))
+        elif(cL==hL): # When the concentrated layer is the last hidden layer
+            cW = round((W - d*minw - max(0,hL-3)*(minw+1)*minw - 1)/(minw + 1))
+        else: # When the concentrated layer is somewhere in the middle 
+            cW = round((W - d*minw - max(0,hL-3)*(minw+1)*minw - (minw+1) - minw)/(2*minw + 1))
+    
+    # If minw is too high we can end up with negative cW 
+    #   so we enforce at least minw nodes at the concentrated layer 
+    cW = max(cW,minw) 
+    
+    # Convert cL to a 0 base index for array assignment 
+    cL -= 1 
+    
+    # Initialize all layers to minw 
+    layer_nodes = layer_nodes * minw 
+    # Step the concentrated layer up to cW number of nodes
+    layer_nodes[cL] = cW 
+    # Get the weights per layer given the nodes assignment 
+    layer_weights = getLWeights(d,hL,layer_nodes) 
+    # Total number of weights in the network (from input through to output)
+    sum_weights = sum(layer_weights)
+    
+    # Compute a-priori Lbar given Bartlett's definition 
+    lbar = round(sum(np.cumsum(layer_weights)/sum_weights)) 
+    print('Weights: ',str(sum_weights), " with Lbar = ",str(lbar))    
+    
+    # Return both the set of nodes per layer (architecture) along with the resultant Lbar 
+    return (layer_nodes,lbar)
